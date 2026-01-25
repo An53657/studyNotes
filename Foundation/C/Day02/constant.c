e794e3b0043d22ad83c94ac180afe6486be74779
Anderson-lan-lves <121377496+An53657@users.noreply.github.com>
2026-01-25 10:54:34 +0800

    add some files

--- Foundation/C/Day02/constant.c
+++ Foundation/C/Day02/constant.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
+//为了防止控制台。出现乱码
 #include <windows.h>
 
-int main() {
+     int main() {
+        //控制台设置格式    
     SetConsoleOutputCP(CP_UTF8);
-    //输出一个整型
-    // printf("%d" , 12);
+//     //输出一个整型
+    printf("%d" , 12);
 
-    //输出实型
-    // printf("%f" , 3.14);
+//     //输出实型
+//     // printf("%f" , 3.14);
 
-    //输出字符型
-    // printf("%c" , 'A');
+//     //输出字符型
+//     // printf("%c" , 'A');
 
-    //输出字符串
-    // printf("%s" , "HelloWorld");
+//     //输出字符串
+//     // printf("%s" , "HelloWorld");
 
-    // printf("我的学校名字为：%s" , "XXX");
+//     // printf("我的学校名字为：%s" , "XXX");
 
-    printf("我是%s,\n今年%d岁了" , "王一以"  , 18);
-    return 0;
-}
+//     // printf("我是%s,\n今年%d岁了" , "王一以"  , 18);
+
+//     //变量的使用
+//     // int a = 20;
+//     // int b = 50;
+//     // int c = a + b;
+//     // printf("%d" , c);
+
+
+         
+        return 0;
+ }   
