4607917e4bd3cd1233c2e8fd728c5a7809ce55f8
Anderson-lan-lves <121377496+An53657@users.noreply.github.com>
2026-01-25 15:05:00 +0800

    add some files

--- Foundation/C/Day02/variable_demo.c
+++ Foundation/C/Day02/variable_demo.c
@@ -1,9 +1,17 @@
-# include <stdio.h>
+#include <stdio.h>
 #include <windows.h>
 
 int main () {
     
     SetConsoleOutputCP(CP_UTF8);
     // printf("HelloWorld!!");
+    //打印int
+    int a = 202;
+    printf("%d\n" , a);
+
+    //利用sizeof(测量大小)
+    //格式sizeof(变量名/数据类型)
+    printf("%zu" ,sizeof(short));
+
     return 0;
 }
